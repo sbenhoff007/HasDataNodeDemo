268a38956ab7651cce0d0cd63f6ef54cc52d39d0
Shelley Benhoff <shelley.benhoff@gmail.com>
2026-02-20 15:09:16 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# HasDataNodeDemo
+# HasData Node Demo
+
 A simple Node project to get SERP results using the HasData API.
+
+## Setting Up The Project
+
+- Install [node](https://nodejs.org/en/download)
+
+- Get a HasData API key from [hasdata.com](https://hasdata.com)
+
+- Set your HASDATA_API_KEY environment variable
+
+Mac/Linux
+
+```export HASDATA_API_KEY=your_real_key_here```
+
+Windows Powershell
+
+```$env:HASDATA_API_KEY="your_real_key_here"```
+
+Windows CMD
+
+```set HASDATA_API_KEY=your_real_key_here```
+
+## Running The Project
+
+- In a terminal, run the following command:
+
+```node index.js "coffee beans"```
+
+You should see the search results printed in the terminal in JSON format.
---
+++ index.js
@@ -0,0 +1,45 @@
+// Simple HasData Google SERP Light demo
+// Requires Node 18+ (for built-in fetch)
+
+const API_KEY = process.env.HASDATA_API_KEY;
+
+if (!API_KEY) {
+  console.error("Missing HASDATA_API_KEY environment variable");
+  process.exit(1);
+}
+
+// Read search term from command line
+// Example: node index.js "coffee beans"
+const query = process.argv[2];
+
+if (!query) {
+  console.log('Usage: node index.js "search term"');
+  process.exit(1);
+}
+
+const BASE_URL = "https://api.hasdata.com/scrape/google-light/serp";
+
+async function run() {
+  const params = new URLSearchParams({
+    q: query
+  });
+
+  const response = await fetch(`${BASE_URL}?${params.toString()}`, {
+    headers: {
+      "x-api-key": API_KEY
+    }
+  });
+
+  if (!response.ok) {
+    const errorText = await response.text();
+    console.error("Request failed:", errorText);
+    process.exit(1);
+  }
+
+  const data = await response.json();
+
+  // Print full JSON nicely formatted
+  console.log(JSON.stringify(data, null, 2));
+}
+
+run();
